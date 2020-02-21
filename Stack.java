59feb8cdbebffe99c8ebe516166b4a9f50b89919
Grac Tang <48929887+gractang@users.noreply.github.com>
2020-02-20 19:01:44 -0800

    Stack class

---
+++ Stack.java
@@ -0,0 +1,55 @@
+/*************************************************************
+ * Class: Stack
+ * Description: 
+ * A ListNode-based FILO data structure.
+ **************************************************************/
+public class Stack 
+{
+	private ListNode top;
+	
+	/**
+	 * Stack constructor
+	 */
+	public Stack() 
+	{
+		top = null;
+	}
+	
+	/**
+	 * pushes an object onto the Stack and updates the top pointer
+	 * accordingly
+	 * @param obj		the object to push onto the Stack
+	 */
+	public void push(Object obj) 
+	{
+		ListNode prev = top;
+		ListNode ln = new ListNode(obj, prev);
+		top = ln;
+	}
+	
+	/**
+	 * pops the topmost object off the Stack and returns it, then updates
+	 * the top pointer accordingly
+	 * @return the object that was removed
+	 */
+	public Object pop()
+	{
+		if (!isEmpty())
+		{
+			ListNode prevTop = top;
+			top = prevTop.getNext();
+			return prevTop.getValue();
+		}
+		return null;
+	}
+	
+	/**
+	 * checks if the Stack is empty
+	 * @return true if empty false if not
+	 */
+	public boolean isEmpty()
+	{
+		return top == null;
+	}
+	
+}
